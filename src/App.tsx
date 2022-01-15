803fd101d1f7ddf7a9440f3d15f868dd6a952543
Choi Eun Jeong <qhdrn67@naver.com>
2022-01-16 00:25:14 +0900

    React Query part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import GlobalStyle from "./GlobalStyle";
 import Router from "./Router";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen />
     </>
   );
 }
